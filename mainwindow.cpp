c7a5a89954a37ff77352a288c4b14288e13c64bc
admin <emanuele.ardinghi@gmail.com>
2024-01-27 20:24:56 +0100

    init

---
+++ mainwindow.cpp
@@ -0,0 +1,29 @@
+#include "mainwindow.h"
+#include "./ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+
+
+void MainWindow::on_pushButton_clicked()
+{
+    //display setting page
+
+}
+
+
+void MainWindow::on_pushButton_3_customContextMenuRequested(const QPoint &pos)
+{
+
+}
+
